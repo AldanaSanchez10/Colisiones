0c37358650a81a63f9d0219c5d076c368b0bdee8
AldanaSanchez <sanchezbernalaldana@gmail.com>
2023-02-01 21:28:05 -0300

    Resolucion Desafio Colisiones

---
+++ Desafio_Aldana/Assets/Scripts/BumpWall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumpWall : MonoBehaviour
+{
+    [SerializeField] GameObject Wall;
+
+    [SerializeField] Transform[] newPositions;
+
+    [SerializeField] private float tempo = 0f;
+    [SerializeField] private float timeLimit = 2f;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (tempo >= timeLimit)
+        {
+            Destroy(Wall);
+            MoveWall();
+        }
+
+        tempo += Time.deltaTime;
+    }
+
+    private void MoveWall()
+    {
+        GetPositions();
+        Instantiate(newPositions[0], newPositions[0].position, newPositions[0].transform.rotation);
+    }
+
+    public Transform GetPositions()
+    {
+        return newPositions[Random.Range(0, newPositions.Length)];
+    }
+}
---
+++ Desafio_Aldana/Assets/Scripts/Shrinker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrinker : MonoBehaviour
+{
+    [SerializeField] private float currentTime;
+    [SerializeField] private float timeToReset = 0.5f;
+
+    //Activar el trigger desde el inspector para que se cambien los tamaños
+
+    [SerializeField] private bool portalIsTrigger;
+    private void OnTriggerEnter(Collider other)
+    {
+        var harryController = other.GetComponent<HarryController>();
+        if (portalIsTrigger)
+        {
+            if (currentTime <= Time.time && harryController != null)
+            {
+                currentTime = Time.time + timeToReset;
+                harryController.ReduceSize();
+            }
+        }
+        
+        else 
+        {
+            if (!portalIsTrigger)
+            {
+                if (currentTime <= Time.time && harryController != null)
+                {
+                    currentTime = Time.time + timeToReset;
+                    harryController.BackToOgSize();
+                }
+            }
+
+            
+        }
+        
+    }
+
+}
